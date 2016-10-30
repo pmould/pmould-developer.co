c058098254818a0cbb5c7620396ad40b54950e0f
pmould <paul_mould@hotmail.com>
2016-10-30 02:09:14 -0500

    Added about me component

---
+++ src/app/about-me/about-me.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { AboutMeComponent } from './about-me.component';
+
+describe('Component: AboutMe', () => {
+  it('should create an instance', () => {
+    let component = new AboutMeComponent();
+    expect(component).toBeTruthy();
+  });
+});
