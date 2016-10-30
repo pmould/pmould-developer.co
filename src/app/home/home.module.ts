fccc7f5f33da03bc002f48885101d7e7851fcef4
pmould <paul_mould@hotmail.com>
2016-10-30 02:06:04 -0500

    Modified home module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProjectComponent } from './projects/project/project.component';
 
 @NgModule({
   imports: [
