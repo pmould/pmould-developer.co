0209e301c10c90bd31122325bb102804b48c70ea
pmould <paul_mould@hotmail.com>
2016-10-31 01:29:18 -0500

    Modified PostListComponent

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  group,
+  ElementRef
+} from '@angular/core';
 import { Post } from '../post';
 import { PostsService } from '../posts.service';
 import { Router } from '@angular/router';
@@ -7,13 +17,79 @@ import { Router } from '@angular/router';
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
-  providers: [PostsService]
+  providers: [PostsService],
+  animations: [
+    trigger('animateTitle', [
+      state('in', style({overflow: 'initial', opacity: 1})),
+      transition('void => *', [
+        style({overflow: 'hidden', opacity: 0}),
+        group([
+          animate('0.5s .75s ease', style({
+            overflow: 'hidden'
+          }))
+        ])
+      ]),
+    ]),
+    trigger('flyInOut', [
+      state('in', style({width: 515, transform: 'translateX(0)', opacity: 1})),
+      transition('void => *', [
+        style({width: 0, padding: 0, border: 'none',overflow: 'hidden', transform: 'translateX(50px)', opacity: 0}),
+        group([
+          animate('0.5s .75s ease', style({
+            transform: 'translateX(0)',
+            width: 515,
+            padding: '20px 25px 25px 25px',
+            border: '5px solid #00a0d4',
+            overflow: 'initial'
+          })),
+          animate('0.5s ease', style({
+            opacity: 1
+          }))
+        ])
+      ]),
+      transition('* => void', [
+        group([
+          animate('0.5s ease', style({
+            transform: 'translateX(50px)',
+            width: 0
+          })),
+          animate('0.5s .75s ease', style({
+            opacity: 0
+          }))
+        ])
+      ])
+    ]),
+    trigger('flyInOutBottom', [
+      state('in', style({
+        transform: 'translateY(0)',
+        borderTop: '1px solid #cdcdcd'
+      })),
+      transition('void => *', [
+        style({
+          transform: 'translateY(200%)',
+          borderTop: 'none'
+        }),
+        animate('0.5s .75s')
+      ]),
+      transition('* => void', [
+        animate('0.5s .75s', style({transform: 'translateY(100%)'}))
+      ])
+    ])
+  ]
 })
 export class PostListComponent implements OnInit {
 
   posts: Post[];
 
-  constructor( private postsService: PostsService, private router: Router ) { }
+  constructor(
+    private postsService: PostsService,
+    private router: Router,
+    private element: ElementRef
+     ) {
+    this.router.events.subscribe((path) => {
+      element.nativeElement.scrollIntoView();
+    });
+  }
 
   getPosts(){
     this.postsService
@@ -25,6 +101,7 @@ export class PostListComponent implements OnInit {
 
   ngOnInit() {
     this.getPosts();
+    $('.navbar').css('background', 'white');
   }
  
   selectPost(slug) {
