3335c6ee306e3e23c247de1e48dac99a54d46f3a
pmould <paul_mould@hotmail.com>
2016-10-31 01:27:43 -0500

    modified HeaderComponent

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,9 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({transform: 'translateY(0)'})),
+      transition('void => *', [
+        style({transform: 'translateY(-100%)'}),
+        animate('.350s .75s')
+      ]),
+      transition('* => void', [
+        animate('.350s .75s', style({transform: 'translateY(100%)'}))
+      ])
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
   siteLogo = 'assets/img/pmould-logo.png';
@@ -11,7 +31,7 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
   }
-  
+
   scrollDown() {
     $('html, body').animate({
       scrollTop: $(".gallery").offset().top
